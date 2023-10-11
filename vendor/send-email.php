2e240c592bc3d4a51eddec7f4ddf5b38c72fa942
shahadkariyambath <shahadchikku@gmail.com>
2023-10-11 12:00:15 +0100

    Shahad Resume

---
+++ vendor/send-email.php
@@ -0,0 +1,36 @@
+<?php
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+
+require "autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+$mail = new PHPMailer(true);
+
+// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "smtp.gmail.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+
+$mail->Username = "shahadchikku@gmail.com";
+$mail->Password = "tsff tqso tose ajyl";
+
+$mail->setFrom($_POST['email'], $name);
+$mail->addAddress("shahadchikku@gmail.com", "Shahad");
+
+$mail->Subject = $subject;
+$mail->Body = "Requestor email id : " . $_POST['email'] . "\n" . $message;
+
+$mail->send();
+
+echo ("email sent");
+// header("Location: sent.html");
