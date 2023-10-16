c953dd3db8526238d90c5af31278f08a9bf7dd6b
shahadkariyambath <shahadchikku@gmail.com>
2023-10-16 13:09:03 +0100

    Made changes i n the js file

--- vendor/send-email.php
+++ vendor/send-email.php
@@ -28,7 +28,7 @@ $mail->setFrom($_POST['email'], $name);
 $mail->addAddress("shahadchikku@gmail.com", "Shahad");
 
 $mail->Subject = $subject;
-$mail->Body = "Requestor email id : " . $_POST['email'] . "\n" . $message;
+$mail->Body = "Requestor name :" . $name . "\nEmail id : " . $_POST['email'] . "\nSubject : " . $subject . "\nMessage :" . $message;
 
 $mail->send();
 
